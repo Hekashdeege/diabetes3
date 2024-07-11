d63c13dadb1be60780b5c2b27f3fa6d9fb9fe045
Hekashdeege <158147156+Hekashdeege@users.noreply.github.com>
2024-07-11 08:43:26 +0530

    Update and rename hhhh.py to hhhh2.py

--- hhhh2.py
+++ hhhh2.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
